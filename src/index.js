d7b6769735464ab4679cf79ddeb4ecb735851dfc
Chandan Singh <chandan.singh7193@gmail.com>
2018-08-30 11:27:29 +0530

    Routing done in ssr using browserrouter in client side and staticrouter on serverside

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 
 import express from 'express';
 import renderer from './helpers/renderer';
-console.log(renderer);
+
 const app = express();
 
 app.use(express.static('public'));
 
-app.get('/',(req,res)=>{
-  res.send(renderer());
+app.get('*',(req,res)=>{
+  res.send(renderer(req));
 })
 
 app.listen(3000,()=>{
